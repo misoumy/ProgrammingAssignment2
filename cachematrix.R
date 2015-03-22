13911c455e177bd69739787091d2b20b8f7e0f40
Mike Soumy <misoumy@users.noreply.github.com>
2015-03-22 17:41:20 +0000

    initial relase

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R, 3/22/2015
+## Definition of two functions, one generating an object capable of storing a matrix and a cached
+## inverse of this matrix. The other function simply computes the inverse providing that it was
+## not computed already (= cached).
+##
+## Example:
+##  matrix <- matrix(c(3,0,0,0,2,0,0,0,1), nrow=3, ncol=3)  # 3x3 matrix ()
+##  obj <- makeCacheMatrix()                                # initialize "empty" object
+##  obj$set(matrix)                                         # storing matrix into the object
+##  obj$get()                                               # reading the stored matrix
+##  obj$setinverse(solve(matrix))                           # storing the inverse 
+##  obj$getinverse()                                        # reading the stored inverse
+##  cacheSolve(obj)                                         # prints cached data
+##
+## Caveats:
+##  - no checking whether the matrix is regular or not.
+##  - fails for singular matrices
 
-## Write a short comment describing this function
 
+## This function generates a set of closures, i.e. a set of internal functions. The objective
+## was to have the ability of storing and reading a matrix together with storing and reading
+## its inverse. Upon calling makeCacheMatrix, a list with these internal functions is returned.
+## 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'. Use the getters defined in makeCacheMatrix in order
+## to read the data stored in the object. The setters, in fact, only the 'setinverse', are used
+## in the event of running the function over an object which already has the inverse computed.
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
